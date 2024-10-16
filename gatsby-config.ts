46d74d1aa6af11e0a417a627d4dbd24f3cc4d661
prometheansacrifice <3097018+ManasJayanth@users.noreply.github.com>
2024-10-16 13:24:59 +0530

    More site meta fixes

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -40,7 +40,7 @@ const config: GatsbyConfig = {
           },
           {
             name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `https://sanya-mittal.com`,
           },
         ],
       },
@@ -54,9 +54,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Sanya Mittal | Business Analyst`,
+        short_name: `sanya-mittal`,
+        description: `I'm Sanya Mittal, and this is my website. I write about what I learn and experience.`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
@@ -123,7 +123,7 @@ const config: GatsbyConfig = {
   }
 }`,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `Sanya Mittal | Business Analyst`,
           },
         ],
       },
